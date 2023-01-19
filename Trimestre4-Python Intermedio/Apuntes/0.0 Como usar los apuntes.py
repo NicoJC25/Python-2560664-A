4a4f94b3081accda636904c66c77a02e96603116
NicoJC25 <nejuez@misena.edu.co>
2023-01-18 22:18:51 -0500

    Creacion de nomenclatura y mas organizacion tanto para los apuntes como para los ejemplos

---
+++ Trimestre4-Python Intermedio/Apuntes/0.0 Como usar los apuntes.py
@@ -0,0 +1,10 @@
+#Este es un archivo para saber como usar los archivos
+
+#Para empezar, los textos que traen el "#" de primeras como este mismo, es teoria del tema.
+#La teoria con esta flecha "-->" al final, significan que la siguiente linea es una continuacion del tema.
+#Los textos encerrados entre '''''' son ejemplos de codigo, solo que se encierran asi para que no se ejecuten junto con otros ejemplos.
+#En la carpeta de ejemplos, los codigos estan encerrados entre comillas, se pueden quitar si se desea para saber como funciona esa parte de codigo.
+#Los apuntes estan con una nomenclatura que indican el orden en que deben ser leidos.
+
+#Nada mas por el momento, adios ;)
+
