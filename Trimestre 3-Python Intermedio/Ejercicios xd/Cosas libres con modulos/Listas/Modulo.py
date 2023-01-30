333fe318209de8cc24db9057ae116e29c921bd26
NicoJC25 <nejuez@misena.edu.co>
2023-01-29 23:06:06 -0500

    Finalizado (por ahora) modulo de listas

--- Trimestre 3-Python Intermedio/Ejercicios xd/Cosas libres con modulos/Listas/Modulo.py
+++ Trimestre 3-Python Intermedio/Ejercicios xd/Cosas libres con modulos/Listas/Modulo.py
@@ -2,27 +2,34 @@ from random import random as ran, randint as rang, randrange as rang2
 
 def lista_random (lista):
     lista=[round(rang2(100))for i in range (rang(10,25))]
+    print('Esta es la lista:')
     return lista
-
 def sumar_lista (lista):
     suma=0
     rango=len(lista)
     for i in range(rango):
         suma+=lista[i]
+    print('La suma de los elementos de la lista es: ')
     return suma
 
 def par (lista):
     par=[x for x in lista if x%2==0]
+    print('Los pares de la lista son: ')
     return par
 
 def impar (lista):
     impar=[x for x in lista if x%2!=0]
+    print('Los impares de la lista son:')
     return impar
 
 def promedio (lista):
-    suma=sumar_lista(lista)
+    suma=0
     rango=len(lista)
+    for i in range(rango):
+        suma+=lista[i]
+    
     promedio=suma//rango
+    print('El promedio de los elementos de la lista es: ')
     return promedio
 
 
--- Trimestre 3-Python Intermedio/Ejercicios xd/Cosas libres con modulos/Listas/Programa.py
+++ Trimestre 3-Python Intermedio/Ejercicios xd/Cosas libres con modulos/Listas/Programa.py
@@ -5,16 +5,28 @@ lista=lista_random(list)
 try:
     while True:
         interfaz ('cls')
-        print ('Bienvenido al menú, elija una opcion:\n 1. Crear una lista aleatoria\n 2. Sumar los datos de la lista\n 3. Mostrar los pares de la lista \n 4. Mostrar los impares de la lista \n 5. Mostrar el promedio de los elementos \n 6. Finalizar el programa \n')
+        print ('Bienvenido al menú, elija una opcion:\n 1. Crear una lista aleatoria\n 2. Sumar los datos de la lista\n 3. Mostrar los pares de la lista \n 4. Mostrar los impares de la lista \n 5. Mostrar el promedio de los elementos \n 6. Mostrar la lista \n 7. Finalizar el programa')
         pedir=int(input(' '))
-        
         match pedir:
             case 1:
+                print(lista_random(lista))
+            case 2:
                 print(sumar_lista(lista))
+            case 3:
+                print(par(lista))
+            case 4:
+                print(impar(lista))
+            case 5:
+                print(promedio(lista))
+            case 6:
+                print(lista)
+            case 7:
+                break
+            case _:
+                print('El numero no es valido')
+        interfaz ('pause')
+except:
+    print('Hubo un error desconocido, vuelve a intentarlo')
                 
-print(lista)
 
 
-print(par(lista))
-print(impar(lista))
-print(promedio(lista))
