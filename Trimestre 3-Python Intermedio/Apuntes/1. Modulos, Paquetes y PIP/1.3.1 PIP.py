69a90c6336b1bef0dd838d7fc97f88c5050c1b39
NicoJC25 <nejuez@misena.edu.co>
2023-02-26 21:13:48 -0500

    Apuntes sobre paquetes PIP

---
+++ Trimestre 3-Python Intermedio/Apuntes/1. Modulos, Paquetes y PIP/1.3.1 PIP.py
@@ -0,0 +1,36 @@
+#Los paquetes PIP son unos paquetes que vienen por asi decirlo hechos desde antes
+#Se pueden encontrar en una pagina en especifico:
+
+#https://pypi.org/.
+
+#A continuacion unos comandos para la instalacion de PIP y los paquetes que uno quiera:
+#Nota: Todo hacerlo desde el simbolo del sistema (CMD)
+
+
+#pip --version
+#(version del instalador)
+
+#pip help
+#(comandos de pip)
+
+#pip list
+#(lista de los paquetes instalados)
+
+#pip show nombre_del_paquete
+#(informacion de un paquete en especifico ya instalado)
+
+#pip show pip
+#(informacion del instalador)
+
+#pip search anystring
+#(buscar paquetes)
+
+#pip install pygame
+#(forma de instalar un paquete, en este caso pygame. Para instalar otro, se reemplaza el nombre del paquete)
+
+#pip install --user pygame
+#(si no tienes permiso de administrador o para instalarlo solo en ese usuario)
+
+#pip uninstall nombre_del_paquete
+#(desinstalar un paquete)
+
